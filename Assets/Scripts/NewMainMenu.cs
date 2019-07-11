ce61ec00d538d030fe932b6c740330decf6454ac
Connor Nagel <connornagel94@gmail.com>
2019-07-10 23:45:58 -0700

    Added Multiplayer option to menu
    Added multiplayer menu object
    Updated menu and camera layout
    Changed menu script to reflect layout

--- Assets/Scripts/NewMainMenu.cs
+++ Assets/Scripts/NewMainMenu.cs
@@ -10,11 +10,12 @@ using Photon.Realtime;
 
 public class NewMainMenu : MonoBehaviourPunCallbacks { 
 
-    public enum MenuSelect { PLAY, OPTIONS, EXIT};
+    public enum MenuSelect { PLAY, MULTI, OPTIONS, EXIT};
     
 
     
     public GameObject PlaySelected;
+    public GameObject MultiSelected;
     public GameObject OptionsSelected;
     public GameObject ExitSelected;
     
@@ -53,46 +54,72 @@ public class NewMainMenu : MonoBehaviourPunCallbacks {
 	void FixedUpdate () {
         if (isActive)
         {
-            float tempInput = Input.GetAxis("L_XAxis_1");
-            if (tempInput > 0 && timeDelay > .2f)
-            {
+            float XInput = Input.GetAxis("L_XAxis_1");
+            float YInput = Input.GetAxis("L_YAxis_1");
+           
                 if (MenuState == MenuSelect.PLAY)
                 {
-                    SelectOptions();
-                    timeDelay = 0f;
-                    MenuState = MenuSelect.OPTIONS;
-                    //AudioManager.instance.Stop("MenuPing");
-                    //AudioManager.instance.Play("MenuPing");
+                    if (YInput > 0 && timeDelay > .2f)
+                    {
+                        SelectMulti();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.MULTI;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
+                }
+                else if (MenuState == MenuSelect.MULTI)
+                {
+                    if (XInput > 0 && timeDelay > .2f)
+                    {
+                        SelectOptions();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.OPTIONS;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
+                    else if (YInput < 0 && timeDelay > .2f) {
+                        SelectPlay();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.PLAY;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
                 }
                 else if (MenuState == MenuSelect.OPTIONS)
                 {
-                    SelectExit();
-                    timeDelay = 0f;
-                    MenuState = MenuSelect.EXIT;
-                    //AudioManager.instance.Stop("MenuPing");
-                    //AudioManager.instance.Play("MenuPing");
-                }
-            }
-            if (tempInput < 0 && timeDelay > .2f)
-            {
-                if (MenuState == MenuSelect.OPTIONS)
-                {
-                    SelectPlay();
-                    timeDelay = 0f;
-                    MenuState = MenuSelect.PLAY;
-                    //AudioManager.instance.Stop("MenuPing");
-                    //AudioManager.instance.Play("MenuPing");
+                    if (XInput > 0 && timeDelay > .2f)
+                    {
+                        SelectExit();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.EXIT;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
+                    else if (XInput < 0 && timeDelay > .2f) {
+                        SelectMulti();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.MULTI;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
+
                 }
                 else if (MenuState == MenuSelect.EXIT)
                 {
-                    SelectOptions();
-                    timeDelay = 0f;
-                    MenuState = MenuSelect.OPTIONS;
-                    //AudioManager.instance.Stop("MenuPing");
-                    //AudioManager.instance.Play("MenuPing");
+                    if (XInput < 0 && timeDelay > .2f)
+                    {
+                        SelectOptions();
+                        timeDelay = 0f;
+                        MenuState = MenuSelect.OPTIONS;
+                        //AudioManager.instance.Stop("MenuPing");
+                        //AudioManager.instance.Play("MenuPing");
+                    }
                 }
-            }
+            
+            
 
+            //Select Menu Option: Run corresponding program
             if (Input.GetButtonDown("A_1") || Input.GetButtonDown("Submit"))
             {
                 if (MenuState == MenuSelect.PLAY)
@@ -123,14 +150,23 @@ public class NewMainMenu : MonoBehaviourPunCallbacks {
     public void SelectPlay()
     {
         PlaySelected.GetComponent<MeshRenderer>().material = HighlightMaterial;
+        MultiSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
         OptionsSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
         ExitSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
 
+    }
+    public void SelectMulti()
+    {
+        PlaySelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
+        MultiSelected.GetComponent<MeshRenderer>().material = HighlightMaterial;
+        OptionsSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
+        ExitSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
     }
 
     public void SelectOptions()
     {
         PlaySelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
+        MultiSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
         OptionsSelected.GetComponent<MeshRenderer>().material = HighlightMaterial;
         ExitSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
     }
@@ -138,6 +174,7 @@ public class NewMainMenu : MonoBehaviourPunCallbacks {
     public void SelectExit()
     {
         PlaySelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
+        MultiSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
         OptionsSelected.GetComponent<MeshRenderer>().material = NonHighlightMaterial;
         ExitSelected.GetComponent<MeshRenderer>().material = HighlightMaterial;
     }
