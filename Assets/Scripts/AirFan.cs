aa4934b54fa655741eab5bed0c4dbd5f46cc98d4
Wumbomancer <39326014+Wumbomancer@users.noreply.github.com>
2019-07-22 14:44:26 -0700

    Updated park track
    Main menu properly allows players to spawn car and drive around test track on the bottom of the main menu.

---
+++ Assets/Scripts/AirFan.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirFan : MonoBehaviour {
+
+    private bool isActive;
+    private GameObject collisionObject;
+    
+
+    private void Start()
+    {
+        isActive = false;
+        collisionObject = null;
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+		//on collision, add force up. limit speed of vehicle while in air
+        if(isActive)
+        {
+            collisionObject.GetComponentInParent<Rigidbody>().AddForce(0,200,0,ForceMode.Acceleration);
+            Debug.Log(collisionObject.GetComponentInParent<Rigidbody>().gameObject.name);
+        }
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collisionObject = collision.gameObject;
+            isActive = true;
+            
+        }
+        
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        isActive = false;
+
+    }
+}
